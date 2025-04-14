175e8c6f6dd67cd13e9d4d7b64e54ed1e5edd6e6
shayluh <shaygarc08@gmail.com>
2025-04-14 10:17:18 -0500

    Updated BankAccount.Java

--- BankAccount.java
+++ BankAccount.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
-
+import java.util.ArrayList;
+import java.util.List;
 public class BankAccount {
-}
+    private String accountNumber;
+    private double balance;
+    private List<String> transactionHistory;
+    private boolean isActive;
+    public BankAccount(String accountNumber) {
+        this.accountNumber = accountNumber;
+        this.balance = 0.0;
+        this.transactionHistory = new ArrayList<>();
+        this.isActive = true;
+    }
+    public void withdraw(double amount) {
+        if (balance == 0.0){
+            transactionHistory.add("Withdrawl Declined of amount: " + amount)
+        }
+        else {
+            balance -= amount;
+            transactionHistory.add("Withdrawal: -" + amount);
+        }
+    }
+    public void deposit(double amount) {
+        double interest = 0.01 * amount;
+
+        amount += interest;
+        balance += amount ;
+        transactionHistory.add("Deposit: +" + amount);
+    }
+} 
